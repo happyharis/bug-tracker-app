ce8db394591ac46d95a655c0003ce18e41851d98
Haris Samingan <muhamad_haris@hotmail.com>
2022-08-25 17:06:00 +0800

    Add data prop

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -18,21 +18,31 @@ function BoardBody() {
       <div className="bg-indigo-500 h-screen">
         <div className="pt-20"></div>
         <div className="flex flex-row">
-          <Column title="doing" />
-          <Column title="done" />
+          <Column title="doing" data={["hello"]} />
+          <Column title="done" data={["goodbye"]} />
         </div>
       </div>
     </>
   );
 }
 
-function Column({ title }) {
+function Column({ title, data }) {
   return (
-    <div className="p-6 h-96 w-3/12 ml-20 bg-gray-100 rounded-sm shadow-md flex flex-col">
-      <div className="text-l font-medium text-blue-600">{title}</div>
-      <div className="p-6 bg-white rounded-sm flex my-3">Something</div>
-      <div className="p-6 bg-white rounded-sm flex my-3">Something</div>
-    </div>
+    <>
+      <div className="p-6 h-96 w-3/12 ml-20 bg-gray-100 rounded-sm shadow-md flex flex-col">
+        <div className="text-l font-medium text-blue-600">{title}</div>
+        {data.map((content) => {
+          return <Card content={content} />;
+        })}
+      </div>
+    </>
+  );
+}
+function Card({ content }) {
+  return (
+    <>
+      <div className="p-6 bg-white rounded-sm flex my-3">{content}</div>
+    </>
   );
 }
 
