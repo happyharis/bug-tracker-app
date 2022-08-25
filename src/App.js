d1ca91c1a10fa13df85c76c16c66bfaeaaa4d918
Haris Samingan <muhamad_haris@hotmail.com>
2022-08-25 14:19:00 +0800

    Add dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
 import "./App.css";
 import Login from "./Login";
 
@@ -7,23 +8,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="home" element={<Home />} />
+        <Route path="app" element={<Dashboard />} />
       </Routes>
     </div>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
   );
 }
 
