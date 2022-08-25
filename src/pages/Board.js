0be82b0ef81d478fafbbab7f63e78302041ad302
Haris Samingan <muhamad_haris@hotmail.com>
2022-08-25 17:22:39 +0800

    Add useDrop

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DndProvider, useDrag } from "react-dnd";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 function Board() {
@@ -27,12 +27,25 @@ function BoardBody() {
 }
 
 function Column({ title, data }) {
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: "card",
+    drop: () => ({ name: title }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+  const isActive = canDrop && isOver;
+  const bgColor = isActive ? "bg-blue-500" : "bg-gray-100";
   return (
     <>
-      <div className="p-6 h-96 w-3/12 ml-20 bg-gray-100 rounded-sm shadow-md flex flex-col">
+      <div
+        ref={drop}
+        className={`p-6 h-96 w-3/12 ml-20 ${bgColor} rounded-sm shadow-md flex flex-col`}
+      >
         <div className="text-l font-medium text-blue-600">{title}</div>
         {data.map((content) => {
-          return <Card content={content} />;
+          return <Card content={content} key={content} />;
         })}
       </div>
     </>
