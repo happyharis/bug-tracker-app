eca5e5cc2a529a5afbbdf76bc14e06974e30b66e
Haris Samingan <muhamad_haris@hotmail.com>
2022-08-25 08:41:48 +0800

    Add tailwind

--- src/Login.js
+++ src/Login.js
@@ -1,50 +1,7 @@
 import React from "react";
-import "./Login.css";
 
 function Login() {
-  return (
-    <main className="form-signin">
-      <form>
-        <img
-          className="mb-4"
-          src="https://icon-library.com/images/icon-bug/icon-bug-15.jpg"
-          alt=""
-          width="72"
-        />
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" />
-            Remember me
-          </label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Sign in
-        </button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-      </form>
-    </main>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default Login;
