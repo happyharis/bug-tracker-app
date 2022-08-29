3229dc3bf0a65d744c4d8830652805d8cf3af1d3
Haris Samingan <muhamad_haris@hotmail.com>
2022-08-29 17:31:03 +0800

    Add add button for new columns

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,4 +1,4 @@
-import { collection, doc, onSnapshot, query } from "firebase/firestore";
+import { addDoc, collection, doc, onSnapshot, query } from "firebase/firestore";
 import React, { createContext, useEffect, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -14,11 +14,11 @@ function Board() {
     </div>
   );
 }
+const projectId = "YOsEd6rZapnISPZIdoAg";
 export const ProjectContext = createContext();
 
 function BoardBody() {
   const [columns, setColumns] = useState([]);
-  const projectId = "YOsEd6rZapnISPZIdoAg";
   const [title, setTitle] = useState("");
 
   useEffect(() => {
@@ -55,15 +55,68 @@ function BoardBody() {
               <Column key={index} title={column.title} />
             </ProjectContext.Provider>
           ))}
-          <button
-            className={`p-2 max-h-10 w-80 bg-slate-500/20 hover:bg-slate-500/60 rounded-sm text-left ml-3 `}
-          >
-            <p className="font-medium">+ Add another list</p>
-          </button>
+          <AnotherListButton />
         </div>
       </div>
     </>
   );
+}
+
+function AnotherListButton() {
+  const [isAdding, setIsAdding] = useState(false);
+  const [columnTitle, setColumnTitle] = useState("hello");
+
+  const addNewColumn = async () => {
+    try {
+      await addDoc(collection(db, "projects", projectId, "columns"), {
+        title: columnTitle,
+      });
+      setColumnTitle("");
+      setIsAdding(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const AddAnotherList = () => {
+    return (
+      <button
+        onClick={() => setIsAdding(!isAdding)}
+        className={`p-2 max-h-10 w-80 bg-slate-500/20 hover:bg-slate-500/60 rounded-sm text-left ml-3 `}
+      >
+        <p className="font-medium">+ Add another list</p>
+      </button>
+    );
+  };
+
+  const AddAnotherListForm = () => {
+    return (
+      <div
+        className={`p-2 w-80 bg-gray-200 rounded-sm shadow-md flex flex-col text-left ml-3`}
+      >
+        <input
+          className="placeholder:text-slate-400 p-3"
+          placeholder="Enter list title..."
+          onChange={(e) => setColumnTitle(e.target.value)}
+          value={columnTitle}
+        />
+        <div className="flex flex-row">
+          <button
+            className="px-3 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 my-2 "
+            onClick={() => addNewColumn()}
+          >
+            Add list
+          </button>
+          <button
+            className="text-3xl px-3 py-2"
+            onClick={() => setIsAdding(!isAdding)}
+          >
+            x
+          </button>
+        </div>
+      </div>
+    );
+  };
+  return isAdding ? <AddAnotherListForm /> : <AddAnotherList />;
 }
 
 export default Board;
